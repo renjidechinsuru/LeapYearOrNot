7094936c1926234467360f05939b2f6a65235210
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-13 08:55:19 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeapYearOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, reminder;
+            Console.WriteLine("Please enter a year");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            reminder = number % 4;
+            reminder = number % 400;
+
+            if (reminder == 0)
+                Console.WriteLine("{0} is a leap year", number);
+            else
+                Console.WriteLine("{0} is a common year", number);
+
+            Console.ReadKey();
+        }
+    }
+}
